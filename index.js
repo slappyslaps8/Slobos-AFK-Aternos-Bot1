a67e6e580efa64528b514ddc54ca6699bae45b6c
slappyslaps8 <slappyslaps8@gmail.com>
2026-03-29 21:58:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const http = require('http');
-http.createServer((req, res) => {
-  res.write("I'm alive!");
-  res.end();
-}).listen(process.env.PORT || 10000);
 "use strict";
 
 const { addLog, getLogs } = require("./logger");
